09cc0046fa238ad2a5d775c5f79b725675f7ed87
David Colby <davidlcolby@gmail.com>
2021-06-24 13:34:32 -0500

    Updates prior to publishing

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
 
   belongs_to :project
 
-  after_create_commit { broadcast_prepend_to [project, :comments], target: "#{dom_id(project)}_comments" }
+  after_create_commit { broadcast_before_to [project, :comments], target: "#{dom_id(project)}_comments" }
 
   validates_presence_of :body
 end
